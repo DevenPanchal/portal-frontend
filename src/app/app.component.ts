02d15cf886d02bfeb9b5b89253b67cc6425acc68
DevenPanchal <devenrpanchal@gmail.com>
2019-05-05 01:05:40 -0400

    Refactor Navbar + Print user name in welcome message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import{BackendCommunicatorService} from './backend-communicator.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { BackendCommunicatorService } from './backend-communicator.service';
+import { User } from './User.model';
+import {AuthService} from "./services/auth.service";
+import {Router} from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,16 +11,33 @@ import{BackendCommunicatorService} from './backend-communicator.service';
 })
 export class AppComponent {
   title = 'jobportalfrontend';
-  static API_URL="http://localhost:8080";
+  static API_URL = "http://localhost:8080";
+  currentUser: User;
 
-  constructor(private serv: BackendCommunicatorService){}
-
-  onSubmit() {
-
-
+  constructor(public authService: AuthService, public router: Router,private serv: BackendCommunicatorService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    console.log('App Component got the current user', this.currentUser);
   }
 
-  
+  onInit() {
+  }
+  onSubmit() {
+  }
+
+  // logout from the app
+  logOut() {
+    console.log('Logout request received');
+    this.authService.logOut()
+      .subscribe(
+        data => {
+          console.log('The Auth service finished logout');
+          this.router.navigate(['/login']);
+        },
+        error => {
+
+        });
+  }
+
 
 }
 
